59d7adfd694e33cda19273a526fa304ba4213d61
n-katou <102382177+n-katou@users.noreply.github.com>
2022-09-07 17:43:51 +0000

    update

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,45 @@
+class QuestionsController < ApplicationController
+  # 質問一覧表示
+  def index
+    @questions = Question.all
+    # p @questions
+  end
+  
+  # 質問詳細ページ表示
+  def show
+    #p params[:id]
+    @question = Question.find(params[:id])
+    # p @question
+  end
+  
+  #質問の作成
+  def new
+    @question = Question.new
+  end
+  
+  #質問の登録
+  def create
+    # p params
+    @question = Question.new(question_params)
+    @question.save
+    redirect_to @question
+  end
+  
+  #質問の編集
+  def edit
+  end
+  
+  #質問の更新
+  def update
+  end
+  
+  #質問の削除
+  def destroy
+  end
+  
+  private
+  def question_params
+    params.require(:question).permit(:title, :name, :content)
+  end
+  
+end
